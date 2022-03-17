7ec32a57050179d99feef831e1fa94b35a7c3118
365Reasons <101677426+365Reasons@users.noreply.github.com>
2022-03-16 23:03:58 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+const dayOfYear = date => Math.floor((date - new Date(date.getFullYear(), 0, 0)) / 1000 / 60 / 60 / 24);
+
+// document.getElementById("reason").innerHTML = dayOfYear(new Date()) + " dias até agora";
+document.getElementById("reason").innerHTML = "A cada dia que passa, aumenta a certeza que você é o amor da minha vida";
